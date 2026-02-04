42771c9ab2199500b4503ab83a708ff376e7e144
rajee2211 <rajee22111992@gmail.com>
2026-02-04 20:10:29 +0530

    Add files via upload

---
+++ HomeassignmentOddEven.js
@@ -0,0 +1,14 @@
+//Write a JavaScript function named `isOddOrEven` that takes an integer as input and returns `Odd` if the 
+//number is odd and `"Even"` if the number is even. 
+
+//Hints to solve
+//Use an operator that checks divisibility by 2 for evenness, and apply a conditional statement to return "Even" 
+//or "Odd".
+
+let number=8
+if(number%2==0){
+    console.log(number +" "+" is even numer")
+}
+else{
+   console.log(number +" "+" is odd numer") 
+}
