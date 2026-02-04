d577c3cf6ea943852d5ea6d4d4f3107bde8d86bd
rajee2211 <rajee22111992@gmail.com>
2026-02-04 20:10:10 +0530

    Add files via upload

---
+++ HomeassignmentNumbertype.js
@@ -0,0 +1,16 @@
+//Create a JavaScript function that determines if a number is positive, negative, or zero and returns a 
+//corresponding string indicating the type.
+
+//Hints to Solve:  
+//Use a conditional statement to check the number type. If the given number is greater than 0, it is a positive 
+//number and if the given number is less than 0, it is a negative number. If the given number is 0, then the 
+//number is neutral. 
+
+let num=-11
+if(num>0){
+    console.log("Given number is postive numer : "+" "+num)
+}else if(num==0){
+   console.log("Given number is Neutral : "+" "+num)
+} else{
+    console.log("Given number is Negative number : "+" "+num)
+}
