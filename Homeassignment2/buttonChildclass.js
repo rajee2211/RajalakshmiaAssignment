0c454dbfcd3a81eb3bb87e18138b775ec42666ab
rajee2211 <rajee22111992@gmail.com>
2026-03-17 19:01:36 +0530

    Add files via upload

---
+++ Homeassignment2/buttonChildclass.js
@@ -0,0 +1,33 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+var webComponentParentClass_1 = require("./webComponentParentClass");
+var Button = /** @class */ (function (_super) {
+    __extends(Button, _super);
+    function Button() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Button.prototype.click = function () {
+        console.log(" Additional message specific to buttons");
+        _super.prototype.click.call(this);
+    };
+    return Button;
+}(webComponentParentClass_1.WebComponent));
+exports.Button = Button;
+var buttonObj = new Button();
+buttonObj.click();
